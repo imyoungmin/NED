8c32d144debad4a4e9c845f303b25fbe613d3b2f
Luis Ángel (임 영민) <ultimate_luis@hotmail.com>
2019-04-25 21:28:58 -0700

    Added check for lists and for avoiding first line of text.  Checking for empty documents.  Checking for URLs in tokens.

--- WikiParser/Parser.py
+++ WikiParser/Parser.py
@@ -10,6 +10,8 @@ from multiprocessing import Pool
 from nltk.corpus import stopwords
 import pymongo
 from pymongo import MongoClient
+from urllib.parse import unquote
+import html
 import Tokenizer
 importlib.reload( Tokenizer )
 
@@ -28,12 +30,14 @@ _UrlProtocols = [
 _FilenamePattern = re.compile( r"^wiki_\d+\.bz2$", re.I )										# Checking only the right files.
 _DocStartPattern = re.compile( r"^<doc.+?id=\"(\d+)\".+?title=\"\s*(.+)\s*\".*?>$", re.I )		# Document head tag.
 _DisambiguationPattern = re.compile( r"^(.+)\s+\(disambiguation\)$", re.I )						# Disambiguation title pattern.
-_ExternalLinkPattern = re.compile( r"<a.*?href=['\"]((" + r"|".join( _UrlProtocols ) + r")%3A|//).*?>\s*(.+?)\s*</a>", re.I )	# None Wikilinks.
-_LinkPattern = re.compile( r"<a.*?href=\"\s*(.+?)\s*\".*?>\s*(.+?)\s*</a>", re.I )				# Links: internals and externals.
+_ListTitlePattern = re.compile( r"^lists?\s+of", re.I )											# List title pattern.
+_ExternalLinkPattern = re.compile( r"<a.*?href=['\"]((" + r"|".join( _UrlProtocols ) + r")(?:%3a|:)|//).*?>\s*(.*?)\s*</a>", re.I )	# None Wikilinks.
+_LinkPattern = re.compile( r"<a.*?href=\"\s*(.+?)\s*\".*?>\s*(.*?)\s*</a>", re.I )				# Links: internals and externals.
+_UrlPattern = re.compile( r"(?:" + r"|".join( _UrlProtocols ) + r")(?:%3a|:)/.*?", re.I )		# URL pattern not inside a link.
 _PunctuationOnlyPattern = re.compile( r"^\W+$" )
 
 # Undesired tags to remove *before* tokenizing text.
-_undesiredTags = ["<onlyinclude>", "</onlyinclude>", "<nowiki>", "</nowiki>"]
+_undesiredTags = ["<onlyinclude>", "</onlyinclude>", "<nowiki>", "</nowiki>", "<br>", "<br/>"]
 
 # Stop words set: use nltk.download('stopwords').  Then add: "n't" and "'s".
 _stopWords = set( stopwords.words( "english" ) )
@@ -77,8 +81,6 @@ def buildTFIDFDictionary():
 					with bz2.open( fullFile, "rt", encoding="utf-8" ) as bz2File:
 						documents = _extractWikiPagesFromBZ2( bz2File.readlines() )
 
-					_nEntities += len( documents )
-
 					# Use multithreading to tokenize each extracted document.
 					pool = Pool()
 					documents = pool.map( _tokenizeDoc, documents )		# Each document object in its own thread.
@@ -86,7 +88,7 @@ def buildTFIDFDictionary():
 					pool.join()											# Close pool and wait for work to finish.
 
 					# Update DB collections.
-					_updateTFIDFCollections( documents )
+					_nEntities += _updateTFIDFCollections( documents )
 
 					endTime = time.time()
 					print( "[**] Done with", file, ":", endTime - startTime )
@@ -169,14 +171,17 @@ def _updateTFIDFCollections( documents ):
 	"""
 	Write collections related to TF-IDF computations.
 	:param documents: A list of dictionaries of the form {id:int, title:str, tokens:{token1:freq1, ..., tokenn:freqn}}.
+	:return: Number of non-empty entity documents processed.
 	"""
 	# Insert Wikipedia titles and documents IDs in entity_id collection.
 	# This serves the purpose of knowing which entities DO exist in the KB.
-	bulkEntities = [{ "_id": doc["id"], "e": doc["title"] } for doc in documents]
+	bulkEntities = [{ "_id": doc["id"], "e": doc["title"] } for doc in documents if doc]		# Skip any empty document.
 	_mEntity_ID.insert_many( bulkEntities )
 
 	# Upsert terms and document frequencies in idf_dictionary collection.
 	for doc in documents:
+		if not doc: continue								# Skip empty documents.
+
 		s = set( doc["tokens"] )  							# Set of terms to upsert.
 		toUpdate = set()  									# Set of term documents IDs to update.
 		for t in _mIdf_Dictionary.find( { "_id": { "$in": list( s ) } } ):
@@ -194,6 +199,8 @@ def _updateTFIDFCollections( documents ):
 	# Bulk insertion of term frequencies for each parsed document.
 	ds = []
 	for doc in documents:
+		if not doc: continue								# Skip empty documents.
+
 		terms = []
 		weights = []
 		for t in doc["tokens"]:
@@ -202,6 +209,7 @@ def _updateTFIDFCollections( documents ):
 		ds.append( {"_id": doc["id"], "t": terms, "w": weights} )
 
 	_mTf_Documents.insert_many( ds )
+	return len( bulkEntities )
 
 
 def _extractWikiPagesFromBZ2( lines ):
@@ -215,32 +223,40 @@ def _extractWikiPagesFromBZ2( lines ):
 
 	extractingContents = False									# On/off when we enter the body of a document.
 	isDisambiguation = False									# On/off when processing a disambiguation document.
+	isList = False
+	firstLine = False											# Skip first line since it's a repetition of the title.
 	for line in lines:
 		line = line.strip()
 		if line == "": continue									# Skip empty lines.
 		if not extractingContents:								# Wait for the sentinel: <doc ...>
 			m = _DocStartPattern.match( line )
 			if m:														# Start of document?
-				md = _DisambiguationPattern.match( m.group( 2 ) )
-				if md:
+				title = html.unescape( m.group( 2 ) )					# Title without html entities.
+				if _DisambiguationPattern.match( title ) is not None:	# Skipping disambiguation pages.
 					isDisambiguation = True
-#					print( "[***] Skipping", m.group(2) )				# Skipping disambiguation pages.
+				elif _ListTitlePattern.match( title ) is not None:		# Skipping list pages.
+					isList = True
 
 				doc = { "id": int(m.group(1)), 							# A new dictionary for this document.
-					    "title": m.group( 2 ),
+					    "title": title,
 						"lines": [] }
 				extractingContents = True								# Turn on the flag: we started reading a document.
+				firstLine = True										# Will be reading title repetion next in the first line.
 			else:
 				print( "Line:", line, "is not in any document!", file=sys.stderr )
 		else:
 			if line == "</doc>":
-				if not isDisambiguation:
+				if not isDisambiguation and not isList:
 					documents += [doc]  								# Add extracted document to list for further processing in caller function.
 				extractingContents = False
 				isDisambiguation = False
-			elif not isDisambiguation:									# Process text within <doc></doc> for non disambiguation pages
-#				_ExternalLinkPattern.sub( r"\3", line )					# Replace external links for their anchor texts.  (No interwiki links affected).
-				doc["lines"].append( line.lower() )						# Add (lowercase) line of text for multithreading.
+				isList = False
+			elif not isDisambiguation and not isList:					# Process text within <doc></doc> for non disambiguation pages.
+				if firstLine:
+					firstLine = False
+				else:
+#					_ExternalLinkPattern.sub( r"\3", line )				# Replace external links for their anchor texts.  (No interwiki links affected).
+					doc["lines"].append( line.lower() )					# Add (lowercase) line of text for multithreading.
 
 	return documents
 
@@ -265,18 +281,19 @@ def _tokenizeDoc( doc ):
 
 		for token in tokens:
 			if len( token ) <= 128:												# Skip too long tokens.
-				if _PunctuationOnlyPattern.match( token ) is None:				# Skip patterns like '...' and '#' and '--'
-					t = _porterStemmer.stem( token, 0, len( token ) - 1 )		# Stem token.
-					if nDoc["tokens"].get( t ) is None:
-						nDoc["tokens"][t] = 1									# Create token in dictionary if it doesn't exist.
-					else:
-						nDoc["tokens"][t] += 1
-					maxFreq = max( nDoc["tokens"][t], maxFreq )					# Keep track of maximum term frequency within document.
+				if _UrlPattern.search( token ) is None:							# Skip URLs.
+					if _PunctuationOnlyPattern.match( token ) is None:			# Skip patterns like '...' and '#' and '--'
+						t = _porterStemmer.stem( token, 0, len( token ) - 1 )	# Stem token.
+						if nDoc["tokens"].get( t ) is None:
+							nDoc["tokens"][t] = 1								# Create token in dictionary if it doesn't exist.
+						else:
+							nDoc["tokens"][t] += 1
+						maxFreq = max( nDoc["tokens"][t], maxFreq )				# Keep track of maximum term frequency within document.
 
 	# Normalize frequency with formula: TF(t,d) = 0.5 + 0.5*f(t,d)/MaxFreq(d).
 	if maxFreq == 0:
-		print( "Empty document!", nDoc, file=sys.stderr )
-		sys.exit( 1 )
+		print( "[W] Empty document:", nDoc, file=sys.stderr )
+		return {}
 
 	for token in nDoc["tokens"]:
 		nDoc["tokens"][token] = 0.5 + 0.5 * nDoc["tokens"][token] / maxFreq
