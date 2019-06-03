741a7c93eb68d5636767ebaa57f891611aa1c295
Luis Ángel (임 영민) <ultimate_luis@hotmail.com>
2019-06-02 23:14:25 -0700

    Remove the label "__main__"

--- Main.py
+++ Main.py
@@ -3,11 +3,9 @@ from Disambiguation import Task as T
 
 importlib.reload( T )
 
+# Disambiguating regular text with labeled named entity mentions.
+results = T.Task.disambiguateTextFile( "Datasets/madonna.txt" )
 
-if __name__ is "__main__":
-	# Disambiguating regular text with labeled named entity mentions.
-	results = T.Task.disambiguateTextFile( "Datasets/madonna.txt" )
-
-	# Disambiguating entities in a big dataset to measure accuracy.
-	# T.Task.debug = False
-	# T.Task.evaluateAccuracy( "Datasets/AIDA-YAGO2-dataset.tsv" )
+# Disambiguating entities in a big dataset to measure accuracy.
+# T.Task.debug = False
+# T.Task.evaluateAccuracy( "Datasets/AIDA-YAGO2-dataset.tsv" )
--- ParseWikipedia.py
+++ ParseWikipedia.py
@@ -11,19 +11,18 @@ _Multistream_Dump = _ROOT + "enwiki-20141106-pages-articles-multistream.xml.bz2"
 _Extracted_XML = _ROOT + "Extracted/"													# Contains extracted XML dumped files.
 _WORD_EMBEDDINGS = _ROOT + "wiki.en.vec"												# Word vectors.
 
-if __name__ is "__main__":
 
-	# Compute and fill collections for SIF and word embeddings.
-	sifParser = SIF.SIFParser()
-	# sifParser.initDBCollections()
-	# sifParser.buildWordEmbeddings( _WORD_EMBEDDINGS )
-	# sifParser.buildSIFDocuments( _Extracted_XML )
-	# sifParser.saveTotalWordCount()
-	# sifParser.saveSIFDocumentsRawEmbeddings()
+# Compute and fill collections for SIF and word embeddings.
+sifParser = SIF.SIFParser()
+# sifParser.initDBCollections()
+# sifParser.buildWordEmbeddings( _WORD_EMBEDDINGS )
+# sifParser.buildSIFDocuments( _Extracted_XML )
+# sifParser.saveTotalWordCount()
+# sifParser.saveSIFDocumentsRawEmbeddings()
 
-	# Compute surface forms and fill collections for NED.
-	# nedParser = NP.NEDParser()
-	# nedParser.initDBCollections()
-	# nedParser.parseSFFromEntityNames()
-	# nedParser.parseSFsAndLsFromWikilinks( _Extracted_XML )
-	# nedParser.parseSFFromRedirectPages( _Multistream_Index, _Multistream_Dump )
+# Compute surface forms and fill collections for NED.
+# nedParser = NP.NEDParser()
+# nedParser.initDBCollections()
+# nedParser.parseSFFromEntityNames()
+# nedParser.parseSFsAndLsFromWikilinks( _Extracted_XML )
+# nedParser.parseSFFromRedirectPages( _Multistream_Index, _Multistream_Dump )
